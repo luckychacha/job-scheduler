5b1e167173707b0ceaa18cb975a50c08d3ae3deb
leixinxin <luckychacha@163.com>
2021-11-10 14:15:10 +0800

    merge into one process

---
+++ scheduler/src/deal_schedule.rs
@@ -0,0 +1,255 @@
+use std::io::Error;
+use std::time::Duration;
+use chrono::Local;
+use tokio::task::JoinHandle;
+use tokio::time;
+
+
+// 每 10 秒扫描一次
+// Todo-List，如果有新任务，通过 add_tasks 方法，将数据写入 HSET 中。
+// 如果是循环任务，就根据 duration 设定一个 interval，并且进行循环，按时间间隔进行打印。
+// 如果是仅一次的任务，就根据 duration 设定一个 sleep，延时打印。
+// 由于时间关系，此处有一个可优化的点，就是每次运行时，可以记录当前的时间【 last tick 】以及执行的总次数【 ran times 】，
+// 用他们可以发现任务未按时执行的情况，实现监控。
+// Running-List 用于处理修改和删除请求。
+// 将任务的 status 改为 STOPPED，保证老任务不会输出。
+// 如果是更新任务，就会把新的任务重新写入 Todo-List，让任务按照新的规则跑起来。
+// 如果是删除任务，就会停止循环。
+pub async fn schedule_start() -> JoinHandle<Result<(), Error>> {
+    tokio::spawn(async move {
+        let mut get_task_interval = time::interval(Duration::from_secs(10));
+        loop {
+
+            get_task_interval.tick().await;
+            async {
+                println!("Start scan tasks in todo-list! now is {}", now());
+
+                if let Ok(tasks) = get_todo_list_from_redis().await {
+                    let _ = add_tasks(tasks).await;
+                }
+
+                println!("Start scan tasks in running-list! now is {}", now());
+
+                if let Ok(tasks) = get_running_list_from_redis().await {
+                    let _ = update_tasks(tasks).await;
+                }
+            }
+            .await;
+        }
+    })
+}
+
+
+async fn get_todo_list_from_redis() -> redis::RedisResult<Vec<String>> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let result: Vec<String> = redis::cmd("LRANGE")
+        .arg(&["todo-list", "0", "-1"])
+        .query_async(&mut con)
+        .await?;
+
+    //RPUSH -> LPOP remove tasks from todo list[todo-list]
+    for task in &result {
+        println!("task {} is removed from todo list", task);
+
+        let _ = redis::cmd("LPOP")
+            .arg(&["todo-list"])
+            .query_async(&mut con)
+            .await?;
+    }
+    Ok(result)
+}
+
+async fn add_tasks(tasks: Vec<String>) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    for task in tasks {
+        let (id, content, schedule_type, duration, slab_idx) = parser_task(task);
+
+        let _ = redis::cmd("HMSET")
+            .arg(&(id))
+            .arg(&["id", &id])
+            .arg(&["content", &content])
+            .arg(&["schedule_type", &schedule_type])
+            .arg(&["duration", &duration.to_string()])
+            .arg(&["status", "RUNNING"])
+            .arg(&["slab_idx", &slab_idx.to_string()])
+            .query_async(&mut con)
+            .await?;
+
+        let _ = tokio::spawn(async move {
+            if "OneShot" == schedule_type {
+                time::sleep(Duration::from_secs(duration)).await;
+                if !check_is_need_to_stop(&id).await {
+                    println!(
+                        "  ****  id: {}, content: {}, type: {} now is {}  ****  ",
+                        id,
+                        content,
+                        schedule_type,
+                        now()
+                    );
+                }
+                // stop_task(&id);
+            } else if "Repeated" == schedule_type {
+                let mut interval = time::interval(Duration::from_secs(duration));
+                loop {
+                    interval.tick().await;
+                    if check_is_need_to_stop(&id).await {
+                        break;
+                    }
+                    async {
+                        println!(
+                            "  ****  id: {}, content: {}, type: {}, now is {}  ****  ",
+                            id,
+                            content,
+                            schedule_type,
+                            now()
+                        );
+                    }
+                    .await;
+                }
+            }
+        });
+    }
+    Ok(())
+}
+
+async fn check_is_need_to_stop(id: &str) -> bool {
+    let job_status = get_job_status(id).await;
+
+    match job_status {
+        Ok(job_status) => {
+            if "RUNNING" == &job_status {
+                return false;
+            }
+            true
+        }
+        Err(_) => true,
+    }
+}
+
+async fn get_job_status(id: &str) -> redis::RedisResult<String> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+    let result: String = redis::cmd("HGET")
+        .arg(&[id, "status"])
+        .query_async(&mut con)
+        .await?;
+    Ok(result)
+}
+
+async fn update_tasks(tasks: Vec<String>) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+    for task in tasks {
+        let (update_id, schedule_type, content) = parse_running_task(task);
+
+        // 无论删除还是修改，都需要把任务停止
+        stop_task(&update_id).await?;
+
+        if "update" == &schedule_type {
+            let (_, content, schedule_type, sec, slab_idx) = parser_task(content);
+
+            let new_job = format!(
+                "{}::{}::{}::{}::{}",
+                update_id, content, schedule_type, sec, slab_idx
+            );
+
+            let _ = redis::cmd("RPUSH")
+                .arg(&["todo-list", &new_job])
+                .query_async(&mut con)
+                .await?;
+        }
+    }
+    Ok(())
+}
+
+async fn stop_task(id: &str) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let _ = redis::cmd("HSET")
+        .arg(&id)
+        .arg("status")
+        .arg("STOPPED")
+        .query_async(&mut con)
+        .await?;
+
+    Ok(())
+}
+
+async fn get_running_list_from_redis() -> redis::RedisResult<Vec<String>> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let result: Vec<String> = redis::cmd("LRANGE")
+        .arg(&["running-list", "0", "-1"])
+        .query_async(&mut con)
+        .await?;
+
+    //RPUSH -> LPOP remove tasks from running list[running-list]
+    for task in &result {
+        println!("task {} is added into running list", task);
+
+        let _ = redis::cmd("LPOP")
+            .arg(&["running-list"])
+            .query_async(&mut con)
+            .await?;
+    }
+    Ok(result)
+}
+
+
+fn now() -> String {
+    let fmt = "%Y-%m-%d %H:%M:%S";
+    Local::now().format(fmt).to_string()
+}
+
+fn parser_task(task: String) -> (String, String, String, u64, i64) {
+    let mut id = String::from("");
+    let mut content = String::from("");
+    let mut schedule_type = String::from("");
+    let mut duration = 0;
+    let mut slab_idx = 0;
+    let _: Vec<&str> = task
+        .split("::")
+        .enumerate()
+        .map(|(idx, item)| {
+            match idx {
+                0 => id = String::from(item),
+                1 => content = String::from(item),
+                2 => schedule_type = String::from(item),
+                3 => duration = item.parse::<u64>().unwrap(),
+                4 => slab_idx = item.parse::<i64>().unwrap(),
+                _ => {}
+            }
+            ""
+        })
+        .collect();
+    (id, content, schedule_type, duration, slab_idx)
+}
+
+fn parse_running_task(task: String) -> (String, String, String) {
+    let mut update_id = String::from("");
+    // ""
+    let mut content = String::from("");
+    // update / delete
+    let mut schedule_type = String::from("");
+
+    let _: Vec<&str> = task
+        .split("|")
+        .enumerate()
+        .map(|(idx, item)| {
+            match idx {
+                0 => update_id = String::from(item),
+                1 => schedule_type = String::from(item),
+                2 => content = String::from(item),
+                _ => {}
+            }
+            ""
+        })
+        .collect();
+    (update_id, schedule_type, content)
+}
---
+++ scheduler/src/server.rs
@@ -0,0 +1,287 @@
+use std::io::Error;
+use std::{collections::BTreeMap};
+
+use poem::{Route, listener::TcpListener};
+use poem_openapi::{payload::Json, ApiResponse, Object, OpenApi, OpenApiService};
+use slab::Slab;
+use tokio::task::JoinHandle;
+use tokio::sync::Mutex;
+use uuid::Uuid;
+
+
+pub async fn http_server_start() -> JoinHandle<Result<(), Error>> {
+    tokio::spawn(async move {
+        // Create a TCP listener
+        let listener = TcpListener::bind("127.0.0.1:3000");
+
+        // Create API service
+        let api_service = OpenApiService::new(Api::default())
+            .title("Hello World")
+            .server("http://localhost:3000/api");
+
+        // Enable the Swagger UI
+        let ui = api_service.swagger_ui();
+
+        // Start the server and specify that the root path of the API is /api, and the path of Swagger UI is /
+        poem::Server::new(listener)
+            .await?
+            .run(Route::new().nest("/api", api_service).nest("/", ui))
+            .await
+    })
+}
+
+
+
+#[derive(Debug, Object, Clone, Eq, PartialEq)]
+struct Job {
+    #[oai(read_only)]
+    id: String,
+
+    #[oai(max_length = 64)]
+    content: String,
+
+    schedule_type: String,
+
+    duration: i64,
+
+    #[oai(read_only)]
+    status: String,
+}
+
+#[derive(Debug, Object, Clone, Eq, PartialEq)]
+struct UpdateJob {
+    content: Option<String>,
+    schedule_type: Option<String>,
+    duration: Option<i64>,
+}
+
+#[derive(ApiResponse)]
+enum CreateJobResponse {
+    /// Returns when the job is successfully created.
+    #[oai(status = 200)]
+    Ok(Json<Job>),
+    #[oai(status = 500)]
+    InternalError,
+}
+
+#[derive(ApiResponse)]
+enum FindJobResponse {
+    /// Return the specified job.
+    #[oai(status = 200)]
+    Ok(Json<Job>),
+    /// Return when the specified job is not found.
+    #[oai(status = 404)]
+    NotFound,
+}
+
+#[derive(ApiResponse)]
+enum DeleteJobResponse {
+    /// Returns when the job is successfully deleted.
+    #[oai(status = 200)]
+    Ok,
+    /// Return when the specified job is not found.
+    #[oai(status = 404)]
+    NotFound,
+}
+
+#[derive(ApiResponse)]
+enum UpdateJobResponse {
+    /// Returns when the job is successfully updated.
+    #[oai(status = 200)]
+    Ok,
+    /// Return when the specified job is not found.
+    #[oai(status = 404)]
+    NotFound,
+}
+
+#[derive(Default)]
+struct Api {
+    jobs: Mutex<Slab<Job>>,
+}
+
+#[OpenApi]
+impl Api {
+    // 创建任务：
+    // 接口接收由 3 个参数组成的 Json 格式对象：
+    // content【 任务内容 】
+    // schedule_type【 任务类型：可重复执行：Repeated / 仅一次：OneShot】
+    // duration【 可重复执行任务时，此参数为循环的时间间隔，仅一次的任务时，此参数为多久后执行 】
+    // 组装数据，为任务生成 id【 uuid 】，以及初始运行状态【 status：RUNNING 】，加上提交的三个参数生成 Job 对象。
+    // 存储：将对象参数存入 slab 中，这个流程用于模拟数据库操作。
+    // 此处因为是 demo，未使用 RDB 这类重型的组件，例如 MySQL，
+    // 后期改造为使用 RDB 如 MySQL 后可以在 Scheduler 中新建一个任务，定时从数据库中查询应执行的任务，
+    // 并且在 Redis 中进行检查，如果不存在就可以进行启动。
+    // 缓存：将存入 slab 返回的 key 以及 Job 对象调用 redis_add_task 方法，写入 Redis 中的 Todo-List 中。
+    #[oai(path = "/jobs", method = "post")]
+    async fn create_job(&self, job: Json<Job>) -> CreateJobResponse {
+        let mut job = job.0;
+        job.id = Uuid::new_v4().to_string();
+        job.status = String::from("RUNNING");
+
+        // Save jobs into slab. It is a demo so if use RDB is too big.
+        // It can also changed to save a record in RDB such as MySQL.
+        let mut jobs = self.jobs.lock().await;
+        let idx = jobs.insert(job.clone()) as i64;
+
+        let res = redis_add_task(&job, "todo-list", idx).await;
+
+        match res {
+            Ok(_) => CreateJobResponse::Ok(Json(job)),
+            Err(_) => CreateJobResponse::InternalError,
+        }
+    }
+
+    // 查询任务：
+    // 接口接收 1 个参数：任务ID【 uuid 】
+    // 通过 任务ID 到 Redis 中的 HSET 查找，如果找到返回任务信息，否则返回 404.
+    #[oai(path = "/jobs/:job_id", method = "get")]
+    async fn index(&self, #[oai(name = "job_id", in = "path")] job_id: String) -> FindJobResponse {
+        match redis_hset_query(&job_id).await {
+            Ok((job, slab_idx)) => FindJobResponse::Ok(Json(job.clone())),
+            Err(_) => FindJobResponse::NotFound,
+        }
+    }
+
+    // 删除任务：
+    // 接口接收 1 个参数：任务ID【 uuid 】
+    // 通过 任务ID 到 Redis 中的 HSET 查找，
+    // 如果找到，就把从 hset 中拿到 slab_idx，根据 slab_id 移除元素，用于模拟数据库的删除操作【物理删除或者逻辑删除】
+    // slab 处理完成后，将数据按照 job_id|delete 的格式写入 Running-List
+    // 否则返回 404.
+    #[oai(path = "/jobs/:job_id", method = "delete")]
+    async fn delete_job(
+        &self,
+        #[oai(name = "job_id", in = "path")] job_id: String,
+    ) -> DeleteJobResponse {
+        match redis_hset_query(&job_id).await {
+            Ok((_, slab_idx)) => {
+                let mut jobs = self.jobs.lock().await;
+                if jobs.contains(slab_idx as usize) {
+                    // remove from slab
+                    jobs.remove(slab_idx as usize);
+
+                    // update cache
+                    let _ = redis_delete(job_id, "running-list").await;
+                    DeleteJobResponse::Ok
+                } else {
+                    DeleteJobResponse::NotFound
+                }
+            }
+            Err(_) => DeleteJobResponse::NotFound,
+        }
+    }
+
+    // 修改任务：
+    // 接口接收 1 个参数：任务ID【 uuid 】，和一个 PUT 传递过来的包含任务内容，任务类型，任务时间三个参数组成的 UpdateJob 对象。
+    // 通过 任务ID 到 Redis 中的 HSET 查找，
+    // 如果找到，就把从 hset 中拿到 slab_idx，根据 slab_id 从更新元素，用于模拟数据库的更新操作
+    // slab 处理完成后，将数据按照 job_id|update|id::content::schedule_type::duration::idx 的格式写入 Running-List
+    // 否则返回 404.
+    #[oai(path = "/jobs/:job_id", method = "put")]
+    async fn put_job(
+        &self,
+        #[oai(name = "job_id", in = "path")] job_id: String,
+        update: Json<UpdateJob>,
+    ) -> UpdateJobResponse {
+        match redis_hset_query(&job_id).await {
+            Ok((job, slab_idx)) => {
+                let mut jobs = self.jobs.lock().await;
+                match jobs.get_mut(slab_idx as usize) {
+                    Some(slab_job) => {
+                        let mut job = job;
+                        if let Some(content) = update.0.content {
+                            job.content = content.clone();
+                            slab_job.content = content;
+                        }
+                        if let Some(schedule_type) = update.0.schedule_type {
+                            job.schedule_type = schedule_type.clone();
+                            slab_job.schedule_type = schedule_type;
+                        }
+                        if let Some(duration) = update.0.duration {
+                            job.duration = duration;
+                            slab_job.duration = duration;
+                        }
+                        let _ = redis_update(&job, "running-list", slab_idx).await;
+                        UpdateJobResponse::Ok
+                    }
+                    None => UpdateJobResponse::NotFound,
+                }
+                // } else {
+                //     UpdateJobResponse::NotFound
+                // }
+            }
+            Err(_) => UpdateJobResponse::NotFound,
+        }
+    }
+}
+
+async fn redis_hset_query(job_id: &str) -> redis::RedisResult<(Job, i64)> {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let cache_query = redis::cmd("HGETALL")
+        .arg(job_id)
+        .query_async(&mut con)
+        .await;
+    match cache_query {
+        Ok(res) => {
+            let res: BTreeMap<String, String> = res;
+            let job: Job = Job {
+                id: res.get("id").unwrap().clone(),
+                content: res.get("content").unwrap().clone(),
+                schedule_type: res.get("schedule_type").unwrap().clone(),
+                duration: res.get("duration").unwrap().parse::<i64>().unwrap(),
+                status: res.get("status").unwrap().clone(),
+            };
+            let slab_idx = res.get("slab_idx").unwrap().parse::<i64>().unwrap();
+            Ok((job, slab_idx))
+        }
+        Err(error) => Err(error),
+    }
+}
+
+async fn redis_add_task(job: &Job, list: &str, idx: i64) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let new_job = format!(
+        "{}::{}::{}::{}::{}",
+        job.id, job.content, job.schedule_type, job.duration, idx
+    );
+    let _ = redis::cmd("RPUSH")
+        .arg(&[list, &new_job])
+        .query_async(&mut con)
+        .await?;
+
+    Ok(())
+}
+
+async fn redis_update(job: &Job, list: &str, idx: i64) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let new_job = format!(
+        "{}::{}::{}::{}::{}",
+        job.id, job.content, job.schedule_type, job.duration, idx
+    );
+    let update_job = format!("{}|update|{}", job.id, new_job);
+    let _ = redis::cmd("RPUSH")
+        .arg(&[list, &update_job])
+        .query_async(&mut con)
+        .await?;
+
+    Ok(())
+}
+
+async fn redis_delete(id: String, list: &str) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await?;
+
+    let delete_job = format!("{}|delete", id);
+    let _ = redis::cmd("RPUSH")
+        .arg(&[list, &delete_job])
+        .query_async(&mut con)
+        .await?;
+
+    Ok(())
+}
